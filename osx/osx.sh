a8ce31ed2bcfa93244ea04edc37e36f0fcc07780
Matt Smith <me@qbyt.me.uk>
2012-07-28 18:38:57 +0100

    Updated osx defaults

--- osx/osx.sh
+++ osx/osx.sh
@@ -13,14 +13,8 @@ defaults write NSGlobalDomain AppleFontSmoothing -int 2
 echo "Automatically hide and show the Dock"
 defaults write com.apple.dock autohide -bool true
 
-echo "Enable iTunes track notifications in the Dock"
-defaults write com.apple.dock itunes-notifications -bool true
-
 # Disable menu bar transparency
 defaults write NSGlobalDomain AppleEnableMenuBarTransparency -bool false
-
-echo "Show all filename extensions in Finder"
-defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
 echo "Use current directory as default search scope in Finder"
 defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
@@ -78,9 +72,6 @@ echo "Enable snap-to-grid for desktop icons"
 
 echo "Disable the warning before emptying the Trash"
 defaults write com.apple.finder WarnOnEmptyTrash -bool false
-
-# Empty Trash securely by default
-defaults write com.apple.finder EmptyTrashSecurely -bool true
 
 echo "Require password immediately after sleep or screen saver begins"
 defaults write com.apple.screensaver askForPassword -int 1
